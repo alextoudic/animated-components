551a00db510ac78d7eb496c81d73efa676452e22
Alex Toudic <alexandre.toudic@gmail.com>
2018-02-19 17:11:08 +0100

    :ambulance: fix export

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import AnimatedGroup from './AnimatedGroup'
 import Animation from './Animation'
 import CSSAnimation from './CSSAnimation'
 
-export AnimatedChange
-export AnimatedGroup
-export Animation
-export CSSAnimation
-
-export default {
+module.exports = {
   AnimatedChange,
   AnimatedGroup,
   Animation,
